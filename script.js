94a1d9e19d46b3841b52b1fab27c4f2b5a947eff
sanjoythakur990 <83492275+sanjoythakur990@users.noreply.github.com>
2023-12-18 14:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i=shortHex.length-3;      // starting index
+	let isCapital=false;
+	let output="#";
+	while(i<shortHex.length){
+		let code=shortHex.charCodeAt(i);
+		if(code>=65 && code<=90) isCapital=true;
+		output+= shortHex[i] + shortHex[i];
+	}
+	return isCapital ? output.toUpperCase() : output;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
